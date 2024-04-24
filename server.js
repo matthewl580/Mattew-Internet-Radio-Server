808b9f3a578c95191ee74d03dfdcf9336047655e
Shiba-2439 <85137306+Shiba-2439@users.noreply.github.com>
2024-04-23 19:27:30 -0500

    Update server.js | ATTEMT TO FIX CORRS - TEST ONE /getTrackInfomation

--- server.js
+++ server.js
@@ -383,6 +383,24 @@ fastify.post("/addTrack", function (request, reply) {
 
 // Returns the entire trackObject
 fastify.get("/getTrackInfomation", function (request, reply) {
+  // CORRS FIX TEXT
+   reply.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://wildflower-radio.glitch.me"
+  );
+  reply.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  reply.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  reply.setHeader("Access-Control-Allow-Credentials", true);
+  reply.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  reply.setHeader("Access-Control-Max-Age", 7200);
+
   return trackObject;
 });
 // Returns only the position of the track
